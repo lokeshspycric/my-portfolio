06019a432117be64be7ad1660d657c5ab93ab28b
lokeshspycric <lokeshdumpa95@gmail.com>
2025-03-20 23:09:52 +0530

    Initial commit

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,10 @@
+export default function Navbar() {
+    return (
+      <nav className="flex justify-around items-center bg-black text-white p-4">
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    )
+  }
+  
